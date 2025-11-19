c4e654bcb5ce3fb12f6ed88fb7e74cf5c014ada1
Tom Li <leezhuosong@gmail.com>
2025-11-19 12:33:26 -0800

    chore: switch SW to network first

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tom-restaurant-v1';
+const CACHE_NAME = 'tom-restaurant-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -28,7 +28,20 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
